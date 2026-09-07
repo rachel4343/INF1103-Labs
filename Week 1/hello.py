26aabfadc5f98180fc05d3bacd83afb2e65f3077
Rachel <2604343@sit.singaporetech.edu.sg>
2026-09-07 20:01:53 +0800

    First Post

---
+++ Week 1/hello.py
@@ -0,0 +1,92 @@
+# Activity 1 : Create your First Python Program and add it to the repository
+print("=============================")
+print("Welcome here")
+print("My first post!")
+print("=============================")
+
+# git add hello.py; Prepares your changes to be committed.
+# git status; You should see your Python file as modified. Checks the current state of your repository.
+# git commit -m "First Post"; "Save this version"
+# git log; Shows your commit history.
+# q; quit
+
+
+
+# # Activity 2: Update your Profile and add it to the repository 
+# username = "cool_creator"
+# bio = "Fun Blogger"
+# followers = 100
+
+# # git diff; Checks the difference in the repository files
+# # git add hello.py; Prepares your changes to be committed.
+# # git status; You should see your Python file as modified. Checks the current state of your repository.
+# # git commit -m "Add user profile information"; "Save this version"
+# # git log; Shows your commit history.
+# # q; quit
+
+
+
+# # Activity 3: Follower Growth tracker 
+# followers += 50  # Increment followers by 50
+# print("Day 1:", followers)
+
+# followers += 20  # Increment followers by 20
+# print("Day 2:", followers)
+
+# followers -= 10 # Decrement followers by 10
+# print("Day 3:", followers)
+
+# print("Username:", username)
+# print("Bio:", bio)
+# print("Followers:", followers)
+
+# # git status; You should see your Python file as modified. Checks the current state of your repository.
+# # git add hello.py; Prepares your changes to be committed.
+# # git commit -m "Update follower count operations"; "Save this version"
+# # git log; Shows your commit history.
+# # q; quit
+
+
+
+# # Activity 4: Interactive profile creator 
+# username = input("Enter your username: ")
+# age = int(input("Enter your age: "))
+# category = input("Enter Content Category: ")
+
+# print("\nInstagram Profile Created!")
+# print("=============================")
+# print("Username:", username)
+# print("Age:", age)
+# print("Content Category:", category)
+
+# # git status; You should see your Python file as modified. Checks the current state of your repository.
+# # git add hello.py; Prepares your changes to be committed.
+# # git commit -m "Create interactive profile"; "Save this version"
+# # git log; Shows your commit history.
+# # q; quit
+
+
+
+# # Activity 5: Something fun to think about.
+# # input() always returns a string (str).
+# # 2 conditions being checked.
+# username = input("Enter your username: ")
+# age = int(input("Enter your age: "))
+# category = input("Enter Content Category: ")
+
+# print("\nInstagram Profile Created!")
+# print("=============================")
+# print("Username:", username)
+# print("Age:", age)
+# print("Content Category:", category)
+
+# if age>40 and category == "fun":
+#     print("You are old what is fun for you??")
+# # elif age<40 and category == "fun":
+# #     print("You are young, have fun!")
+
+# # git status; You should see your Python file as modified. Checks the current state of your repository.
+# # git add hello.py; Prepares your changes to be committed.
+# # git commit -m "Add age and category condition"
+# # git log; Shows your commit history.
+# # q; quit
