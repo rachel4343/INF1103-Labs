dd33f7dd85b08d27e344688fbf291d06057e0df5
Rachel <2604343@sit.singaporetech.edu.sg>
2026-09-07 20:14:25 +0800

    Assignment Variables

--- Week 1/hello.py
+++ Week 1/hello.py
@@ -21,34 +21,30 @@ print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
 
-# # git diff; Checks the difference in the repository files
-# # git add hello.py; Prepares your changes to be committed.
-# # git status; You should see your Python file as modified. Checks the current state of your repository.
-# # git commit -m "Add user profile information"; "Save this version"
-# # git log; Shows your commit history.
-# # q; quit
+# git diff; Checks the difference in the repository files
+# git add hello.py; Prepares your changes to be committed.
+# git status; You should see your Python file as modified. Checks the current state of your repository.
+# git commit -m "Added Profile Variables"; "Save this version"
+# git log; Shows your commit history.
+# q; quit
 
 
 
-# # Activity 3: Follower Growth tracker 
-# followers += 50  # Increment followers by 50
-# print("Day 1:", followers)
+# Activity 3: Follower Growth tracker 
+followers += 50  # Increment followers by 50
+print("Day 1:", followers)
 
-# followers += 20  # Increment followers by 20
-# print("Day 2:", followers)
+followers += 20  # Increment followers by 20
+print("Day 2:", followers)
 
-# followers -= 10 # Decrement followers by 10
-# print("Day 3:", followers)
+followers -= 10 # Decrement followers by 10
+print("Day 3:", followers)
 
-# print("Username:", username)
-# print("Bio:", bio)
-# print("Followers:", followers)
-
-# # git status; You should see your Python file as modified. Checks the current state of your repository.
-# # git add hello.py; Prepares your changes to be committed.
-# # git commit -m "Update follower count operations"; "Save this version"
-# # git log; Shows your commit history.
-# # q; quit
+# git status; You should see your Python file as modified. Checks the current state of your repository.
+# git add hello.py; Prepares your changes to be committed.
+# git commit -m "Update follower count operations"; "Save this version"
+# git log; Shows your commit history.
+# q; quit
 
 
 
