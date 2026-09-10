3e4ffecef167d258f19dfad6e8e1cd5d0aa5a246
Rachel <2604343@sit.singaporetech.edu.sg>
2026-09-10 15:51:28 +0800

    Add continuous stock input loop

--- auditor.py
+++ auditor.py
@@ -1,3 +1,19 @@
 # Requirement 1
 # Initialize the inventory to zero in the start
+# git add auditor.py
+# git commit -m "Initialize inventory"
+# git status
 inventory = 0
+
+# Requirement 2
+# Run in a continuous loop asking user to enter a stock quantity, until the user 
+# types quit. (Think of which loop might be helpful here: for or while) 
+# git add auditor.py
+# git commit -m "Add continuous stock input loop"
+# git status
+while True:
+    stock = input("Enter stock quantity: ")
+    if stock == "quit":
+        break
+
+    
